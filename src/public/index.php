4ce9aeaf4c49fb1e8bc21ef8d7e8a7d1a5522f20
Rikcon <rikcon@yandex.ru>
2021-12-18 16:27:58 +0200

    initial

---
+++ src/public/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <title>Palmo Helloe world</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="manifest" href="site.webmanifest">
+    <meta name="theme-color" content="#fafafa">
+</head>
+
+<body style="margin: 0;">
+
+<!-- Add your site or application content here -->
+<main style="display: flex; justify-content:center; margin-top: 25px;">
+    <img src="palmo.jpg" alt="">
+</main>
+<?php
+    phpinfo()
+?>
+</body>
+
+</html>
+<?php
+phpinfo();
