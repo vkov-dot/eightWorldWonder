5543cc8ef948c6e7b69bfc5fae771d295e60f28d
Vladislav Kovryhin <vkov8212@gmail.com>
2022-02-11 10:12:06 +0200

    add non-activated migrations

---
+++ src/database/migrations/2022_02_10_200257_create_issues_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIssuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('issues', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 40);
+            $table->string('link', 150);
+            $table->nullableNumericMorphs('heading_id');
+            $table->foreign('heading_id')->references('id')->on('headings');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('issues');
+    }
+}
